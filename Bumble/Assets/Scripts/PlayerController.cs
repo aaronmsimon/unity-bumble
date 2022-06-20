129729953f65ffb4195f9bdf6bf7bcea46778fc0
aaronmsimon <aaronsimon@gmail.com>
2022-06-19 23:14:30 -0700

    Mouse click to move Bumble and add trail renderer

--- Bumble/Assets/Scripts/PlayerController.cs
+++ Bumble/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float maxTrailTime = 1f;
+
+    private Camera cam;
+    private Vector3 targetPos;
+
+    private TrailRenderer trail;
+
     void Start()
     {
-        
+        cam = Camera.main;
+        trail = gameObject.GetComponentInChildren<TrailRenderer>();
+        trail.time = maxTrailTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -cam.transform.position.z);
+            targetPos = cam.ScreenToWorldPoint(mousePos);
+        }
+
+        trail.enabled = Vector3.Distance(transform.position, targetPos) / speed <= maxTrailTime;
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
     }
 }
