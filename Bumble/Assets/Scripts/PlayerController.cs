7809e78ca7112bfee9416090e1961dc574c9b112
aaronmsimon <aaronsimon@gmail.com>
2022-06-20 22:25:17 -0700

    Add Bezier curve, check for normal position of curve handle and repeat call

--- Bumble/Assets/Scripts/PlayerController.cs
+++ Bumble/Assets/Scripts/PlayerController.cs
@@ -10,9 +10,14 @@ public class PlayerController : MonoBehaviour
     private Camera cam;
     private Vector3 targetPos;
 
+    private float bumbleTime = 1f;
+    private Vector3 startPos;
+    private Vector3 midPos;
+    private Vector3 endPos;
+
     private TrailRenderer trail;
 
-    void Start()
+    private void Start()
     {
         cam = Camera.main;
         trail = gameObject.GetComponentInChildren<TrailRenderer>();
@@ -20,7 +25,7 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -28,8 +33,85 @@ public class PlayerController : MonoBehaviour
             targetPos = cam.ScreenToWorldPoint(mousePos);
         }
 
-        trail.enabled = Vector3.Distance(transform.position, targetPos) / speed <= maxTrailTime;
+        //trail.enabled = Vector3.Distance(transform.position, targetPos) / speed <= maxTrailTime;
 
         transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Bumble(5));
+        }
+    }
+
+    private static Vector3 QuadraticCurve(Vector3 a, Vector3 b, Vector3 c, float t)
+    {
+        Vector3 p0 = Vector3.LerpUnclamped(a, b, t);
+        Vector3 p1 = Vector3.LerpUnclamped(b, c, t);
+
+        return Vector3.LerpUnclamped(p0, p1, t);
+    }
+
+    IEnumerator Bumble(int loopCount)
+    {
+        for (int i = 0; i < loopCount; i++)
+        {
+            float minRadius = 4f;
+            float maxRadius = 8f;
+
+            float endRadius = Random.Range(minRadius, maxRadius);
+            float midRadius = Random.Range(minRadius, endRadius);
+
+            startPos = transform.position;
+            endPos = Random.insideUnitCircle.normalized * endRadius + (Vector2)startPos;
+
+            Vector3 dir = endPos - startPos;
+            Vector3 normal = new Vector3(dir.y, -dir.x, 0);
+
+            Vector3 checkLine;
+            Vector3 checkPoint;
+
+            do
+            {
+                midPos = Random.insideUnitCircle.normalized * midRadius + (Vector2)startPos;
+                checkLine = (startPos + normal) - (startPos - normal);
+                checkPoint = midPos - (startPos - normal);
+            }
+            while (Mathf.Sign(Vector3.Cross(checkLine, checkPoint).z) < 0);
+
+            float bumbleSpeed = 1f / bumbleTime;
+            float percent = 0;
+
+            while (percent < 1)
+            {
+                percent += Time.deltaTime * bumbleSpeed;
+                transform.position = QuadraticCurve(startPos, midPos, endPos, percent);
+
+                yield return null;
+            }
+
+            targetPos = endPos;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(startPos, .5f);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(midPos, .5f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(endPos, .5f);
+
+        Vector3 diff = endPos - startPos;
+        Vector3 normal = new Vector3(diff.y, -diff.x, 0);
+        Gizmos.color = Color.white;
+        Gizmos.DrawSphere(startPos + normal, .5f);
+        Gizmos.color = Color.black;
+        Gizmos.DrawSphere(startPos - normal, .5f);
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(startPos, Vector3.Magnitude(normal));
+
+        //Debug.Log("y2+ " + (midPos - normal + startPos));
+        //Debug.Log("y2- " + (midPos - startPos - normal));
     }
 }
